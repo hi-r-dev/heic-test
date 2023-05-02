032469a4a398be9c2bf0a85254214695cce47f85
hi_r_kouzu <hi.r.kouzu@gmail.com>
2023-05-02 14:30:42 +0900

    add image/heic

--- src/FileUpload.tsx
+++ src/FileUpload.tsx
@@ -34,7 +34,11 @@ function FileUpload() {
   return (
     <form onSubmit={onSubmit}>
       <div>
-        <input type="file" capture="camera" onChange={onChange} />
+        <input
+          type="file"
+          accept="image/heic,image/heif,image/*"
+          onChange={onChange}
+        />
       </div>
       <button type="submit">Upload</button>
       {fileName && <p>{fileName}</p>}
