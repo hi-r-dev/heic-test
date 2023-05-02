2b106c81e920821ea4836276d4a18999918ac5a5
hi_r_kouzu <hi.r.kouzu@gmail.com>
2023-05-02 15:03:39 +0900

    display exif data from file

--- src/FileUpload.tsx
+++ src/FileUpload.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
+import exifr from "exifr";
 
 function FileUpload() {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState("");
+  const [exifData, setExifData] = useState(null);
 
-  const onChange = (e: any) => {
+  const onChange = async (e: any) => {
     setFile(e.target.files[0]);
     setFileName(e.target.files[0].name);
+    try {
+      const data = await exifr.parse(e.target.files[0]);
+      setExifData(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const onSubmit = async (e: any) => {
@@ -42,6 +50,12 @@ function FileUpload() {
       </div>
       <button type="submit">Upload</button>
       {fileName && <p>{fileName}</p>}
+      {exifData && (
+        <div>
+          <p>EXIF data:</p>
+          <pre>{JSON.stringify(exifData, null, 2)}</pre>
+        </div>
+      )}
     </form>
   );
 }
